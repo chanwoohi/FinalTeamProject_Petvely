bcf5eca795898fc1d0a97a5e1f67ad28361eb502
ParkChanWooo <chanwoohi@naver.com>
2024-09-30 16:01:33 +0900

    [pcw] VO 추가 중

--- petvely/src/main/java/kr/kh/petvely/model/vo/MemberVO.java
+++ petvely/src/main/java/kr/kh/petvely/model/vo/MemberVO.java
@@ -4,21 +4,21 @@ import java.util.Date;
 
 public class MemberVO {
 
-	int me_num;
-	String me_id; 
-	String me_pw; 
-	String me_nickname;
-	String me_email;
-	String me_authority; 
-	String me_cookie; 
-	Date me_limit; 
-	String me_loginType; 
-	String me_addressDetail; 
-	String me_phone; 
-	int me_wm_point; 
-	int me_ga_point; 
-	int me_mp_point; 
-	int me_total_point; 
-	int me_fakeReportNum; 
-	String me_ms_status;
+	private int me_num;
+	private String me_id; 
+	private String me_pw; 
+	private String me_nickname;
+	private String me_email;
+	private String me_authority; 
+	private String me_cookie; 
+	private Date me_limit; 
+	private String me_loginType; 
+	private String me_addressDetail; 
+	private String me_phone; 
+	private int me_wm_point; 
+	private int me_ga_point; 
+	private int me_mp_point; 
+	private int me_total_point; 
+	private int me_fakeReportNum; 
+	private String me_ms_status;
 }
