6a0bd6f7e8bd8a34151c952b7836de9d357210bf
chanwoohi <chanwoohi@naver.com>
2024-10-21 22:11:58 +0900

    [pcw] 다시 복구했음

---
+++ petvely/src/main/java/kr/kh/petvely/service/member/MemberDetailService.java
@@ -0,0 +1,26 @@
+package kr.kh.petvely.service.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import kr.kh.petvely.dao.member.MemberDAO;
+import kr.kh.petvely.model.user.CustomUser;
+import kr.kh.petvely.model.vo.MemberVO;
+
+@Service
+public class MemberDetailService implements UserDetailsService{
+	// 시큐리티 내가 추가하려고 했을 때 만든 애인데 얘 떄문에 오류 발생?? 잠시 지우자
+	@Autowired
+	MemberDAO memberDao;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		MemberVO member = memberDao.selectMember(username);
+
+		return member == null ? null : new CustomUser(member);
+	}
+
+}
