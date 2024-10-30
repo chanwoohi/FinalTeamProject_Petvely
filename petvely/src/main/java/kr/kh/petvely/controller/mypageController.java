794d91f95f167344a35edf0db49ae8f485e8fa10
hxkyu <hxkyu@naver.com>
2024-10-30 15:25:49 +0900

    마이페이지 펫등록 css중

--- petvely/src/main/java/kr/kh/petvely/controller/mypageController.java
+++ petvely/src/main/java/kr/kh/petvely/controller/mypageController.java
@@ -1,27 +1,46 @@
 package kr.kh.petvely.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kr.kh.petvely.model.user.CustomUser;
+import kr.kh.petvely.model.vo.AnimalVO;
+import kr.kh.petvely.model.vo.MemberVO;
+import kr.kh.petvely.service.AnimalService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @Controller
 @AllArgsConstructor
 public class mypageController {
+	
+	@Autowired
+	private AnimalService animalService;
+	
 	@GetMapping("/mypage/mypage")
 	public String showProfilePage(Model model) {
 		model.addAttribute("currentPage", "user");
 		return "/mypage/mypage";
 	}
 	@GetMapping("/mypage/pet")
-	public String showgetProfilePage(Model model) {
+	public String showgetProfilePage(Model model, @AuthenticationPrincipal CustomUser customUser) {
 		model.addAttribute("currentPage", "pet");
+		
+
+		if(customUser != null) {
+			MemberVO user = customUser.getMember();
+			List<AnimalVO> petList = animalService.selectPetList(user.getMe_num());
+			
+			model.addAttribute("petList", petList);
+		}
+		
 		return "/mypage/pet";
 	}
+
 	
 	
 }
