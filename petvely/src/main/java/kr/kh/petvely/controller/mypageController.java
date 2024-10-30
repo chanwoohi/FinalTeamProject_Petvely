e422ba2909c7116e75240e7b72eb3a8cf9bad302
hxkyu <hxkyu@naver.com>
2024-10-30 13:47:14 +0900

    펫 등록 구현중

--- petvely/src/main/java/kr/kh/petvely/controller/mypageController.java
+++ petvely/src/main/java/kr/kh/petvely/controller/mypageController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -12,7 +14,14 @@ import lombok.AllArgsConstructor;
 public class mypageController {
 	@GetMapping("/mypage/mypage")
 	public String showProfilePage(Model model) {
+		model.addAttribute("currentPage", "user");
 		return "/mypage/mypage";
 	}
+	@GetMapping("/mypage/pet")
+	public String showgetProfilePage(Model model) {
+		model.addAttribute("currentPage", "pet");
+		return "/mypage/pet";
+	}
+	
 	
 }
