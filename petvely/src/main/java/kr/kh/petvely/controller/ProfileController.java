89a6b5b4a64e959b1f861322b1287291ea164f28
kanggun99 <kanggun99@gmail.com>
2024-11-07 14:32:01 +0900

    프로필 수정중

--- petvely/src/main/java/kr/kh/petvely/controller/ProfileController.java
+++ petvely/src/main/java/kr/kh/petvely/controller/ProfileController.java
@@ -1,6 +1,13 @@
 package kr.kh.petvely.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ProfileController{
-       
+ 
+	@GetMapping("/profile/profile")
+	public String updateComment(Model model) {
+		return "profile/profile";
+	}
 	
 }
